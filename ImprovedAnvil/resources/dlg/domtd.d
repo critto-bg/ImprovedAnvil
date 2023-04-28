579243fd6a2899072dbc37499270a5d840de6d18
RD <el.critto@gmail.com>
2023-04-28 04:19:07 +0400

    Fix behavior of the Star card in Deck of the Many Things (consistent targeting, proper class detection)

--- ImprovedAnvil/resources/dlg/domtd.d
+++ ImprovedAnvil/resources/dlg/domtd.d
@@ -1,6 +1,16 @@
-EXTEND_BOTTOM ~DOMT~ 19
-  IF ~OR(2)
-Class(LastTalkedToBy,BARD_ALL)
-Class(LastTalkedToBy,THIEF_ALL)~ THEN GOTO 33
+ALTER_TRANS DOMT
+  BEGIN 19 END
+  BEGIN 0 END
+BEGIN
+  "TRIGGER" ~!Class(LastTalkedToBy,CLERIC_ALL) !Class(LastTalkedToBy,DRUID_ALL) !Class(LastTalkedToBy,SHAMAN) !Class(LastTalkedToBy,MAGE_ALL) !Class(LastTalkedToBy,BARD_ALL) !Class(LastTalkedToBy,THIEF_ALL)~
 END
 
+ALTER_TRANS DOMT
+  BEGIN 19 END
+  BEGIN 1 END
+BEGIN
+  "TRIGGER" ~OR(2)
+Class(LastTalkedToBy,CLERIC_ALL)
+Class(LastTalkedToBy,DRUID_ALL)
+!Class(LastTalkedToBy,SHAMAN)~
+END
