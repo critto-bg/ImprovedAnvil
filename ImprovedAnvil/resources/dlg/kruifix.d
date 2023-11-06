77de3abb1ea8af5679c9cb407f915b1970979f63
RD <el.critto@gmail.com>
2023-11-06 15:28:14 +0400

    patch entrance to cromwell’s shop

--- ImprovedAnvil/resources/dlg/kruifix.d
+++ ImprovedAnvil/resources/dlg/kruifix.d
@@ -1,15 +1,15 @@
 REPLACE KRUIN
   IF ~~ THEN BEGIN 10
   SAY #59831
-  IF ~PartyHasItem("misc8u")~ THEN REPLY #59832 DO ~TakePartyItem("misc8u")~ GOTO 11
-  IF ~PartyHasItem("misc8u")~ THEN REPLY #59833 DO ~TakePartyItem("misc8u")~ GOTO 11
+  IF ~PartyHasItem("misc8u")~ THEN REPLY #59832 DO ~TakePartyItem("misc8u") SetGlobal("IaGaveKruinSword","GLOBAL",1)~ GOTO 11
+  IF ~PartyHasItem("misc8u")~ THEN REPLY #59833 DO ~TakePartyItem("misc8u") SetGlobal("IaGaveKruinSword","GLOBAL",1)~ GOTO 11
   IF ~~ THEN REPLY #59834 GOTO 12
   IF ~~ THEN REPLY #59835 GOTO 12
 END
 
 IF ~~ THEN BEGIN 12
   SAY #59837
-  IF ~PartyHasItem("misc8u")~ THEN REPLY #59838 DO ~TakePartyItem("misc8u")~ GOTO 11
+  IF ~PartyHasItem("misc8u")~ THEN REPLY #59838 DO ~TakePartyItem("misc8u") SetGlobal("IaGaveKruinSword","GLOBAL",1)~ GOTO 11
   IF ~~ THEN REPLY #59839 GOTO 13
 END
 END
