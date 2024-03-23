c715d86895626993c5f0d6c1004c80bfc1890b59
RD <el.critto@gmail.com>
2024-03-23 13:51:55 +0400

    EDE druid helper

---
+++ ImprovedAnvil/resources/dlg/iaspiede.d
@@ -0,0 +1,27 @@
+BEGIN ~IASPIEDE~
+
+IF ~NumTimesTalkedTo(0)~ THEN BEGIN Welcome
+  SAY @5950
+
+  ++ @5951 + Why
+END
+
+IF ~~ THEN BEGIN Why
+  SAY @5952
+
+  ++ @5953 + What
+END
+
+IF ~~ THEN BEGIN What
+  SAY @5954
+  = @5955
+  = @5956
+
+  ++ @5957 + Goodbye
+END
+
+IF ~~ THEN BEGIN Goodbye
+  SAY @5958
+
+  IF ~~ THEN DO ~SetGlobal("Iaspirit","AR4500",2) SetGlobalTimer("Iarakpspawn","AR4500",6) CreateCreatureObjectDoor("S!smiede",Player1,0,0,0) DestroySelf()~ EXIT
+END
